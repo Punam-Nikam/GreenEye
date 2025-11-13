3c172900b3a5d2d5de1eec36a370500f3ae38f44
Punam-Nikam <punamnikam.835@gmail.com>
2025-11-13 16:56:30 +0530

    Create config.py

---
+++ Backend/config.py
@@ -0,0 +1,6 @@
+class MySQLConfig:
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'  # change if your MySQL username is different
+    MYSQL_PASSWORD = 'PASSWORD'  # replace with your actual MySQL password
+    MYSQL_DB = 'greeneye_db'
+    MYSQL_CURSORCLASS = 'DictCursor'
